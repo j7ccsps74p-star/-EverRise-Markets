8767cebc42f168a8cfb94b717073c5fe079d5983
j7ccsps74p-star <j7ccsps74p@privaterelay.appleid.com>
2026-07-31 17:38:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// EverRise Markets JavaScript
+function random(min,max){
+return (Math.random()*(max-min)+min).toFixed(2);
+}
+
+setInterval(function(){
+
+document.getElementById("eurusd").innerHTML=random(1.16,1.19);
+
+document.getElementById("btc").innerHTML="$"+Math.floor(random(117000,120000));
+
+document.getElementById("gold").innerHTML="$"+Math.floor(random(3350,3450));
+
+document.getElementById("nasdaq").innerHTML=Math.floor(random(24000,24500));
+
+},3000);
